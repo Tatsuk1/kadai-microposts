fd8a1a3e6478c7ab9409dbf3debce702e23770af
Tatsuk1 <t.tatsuki0417@gmail.com>
2019-07-09 15:44:24 +0000

    like

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favo_contents, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -27,6 +29,19 @@ class User < ApplicationRecord
     self.followings.include?(other_user)
   end
   
+  def like(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unlike(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(micropost)
+    self.favo_contents.include?(micropost)
+  end
+    
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
